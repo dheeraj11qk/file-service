571b5ba15bdb7fa325fa53ccc1f836e865f21bd0
Dheeraj Gautam <dheerajgautam@WILASSETA0247.local>
2026-03-23 02:10:21 +0530

    old changes

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,12 +14,13 @@ var DB *gorm.DB
 
 func InitDB() {
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
